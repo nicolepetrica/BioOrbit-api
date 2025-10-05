9f2d07e9a5744b00a5a680ac318fa1da01643c58
HeitorHs <heiitorheenrique@gmail.com>
2025-10-05 09:32:05 -0300

    arrumando commit anterior
    deletei o teste e esqueci de mudar as routes

--- http/research-orbits/src/App.tsx
+++ http/research-orbits/src/App.tsx
@@ -3,14 +3,12 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Saved from "./pages/Saved";
-import Teste from "./pages/Teste";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/saved" element={<Saved />} />
-      <Route path="/teste" element={<Teste />} />
     </Routes>
   );
 }
